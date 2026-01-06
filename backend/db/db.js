53a32ddc74d7bfe6355fd4cf6fff6a66aa056dfb
yumi <cyannara@naver.com>
2026-01-06 11:54:09 +0900

    폴더 정리(db폴더 추가)

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./guestbook.db");
+const db = new sqlite3.Database("../guestbook.db");
 
 db.serialize(() => {
   db.run(`
