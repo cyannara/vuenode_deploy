5658e6f5fbb750088c20489b2f3cb4af18b1ccd1
yumi <cyannara@naver.com>
2026-01-07 16:54:14 +0900

    deploy back 3

--- backapp/app.js
+++ backapp/app.js
@@ -10,7 +10,7 @@ app.use("/guestbook", guestbookRouter);
 
 app.get("/", (req, res) => {
   console.log(req.url);
-  res.send("Hello World! 2");
+  res.send("Hello World! 3");
 });
 
 app.listen(port, () => {
